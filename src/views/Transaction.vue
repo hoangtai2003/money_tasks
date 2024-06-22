1b4af6cb1e98481dbeb14d8502b6a8a027f702c3
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2024-06-22 22:29:37 +0700

    Fetch All Data

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -11,26 +11,24 @@
 </template>
 <script>
 export default {
-    data(){
+    data() {
         return {
-            transactions: [
-                {
-                    id: 1,
-                    name: "Đi siêu thị ngày 28/5",
-                    price: 1000000
-                },
-                {
-                    id: 2,
-                    name: "Thanh toán tiền nhà",
-                    price: 2000000
-                },
-                {
-                    id: 3,
-                    name: "Thanh toán tiền điện nước",
-                    price: 3000000
-                }
-            ]
-        }
+            transactions: []
+        };
+    },
+    // Cách 1: sử dụng await vì đây là dữ liệu bất đồng bộ
+    // Sử dụng await trước một Promise nó sẽ tạm dừng thực thi của hàm cho đến khi Promise đó được giải quyết hoặc từ chối
+    async logTransactions() {
+        // Thực thi hàm sẽ tạm dừng ở đây cho đến khi yêu cầu fetch hoàn thành và Promise trả về bởi fetch được giải quyết với đối tượng phản hồi.
+        const response = await fetch("http://localhost:3000/transactions")
+        const data = await response.json()
+        this.transactions = data
     }
+    // Cách 2
+//     created() {
+//         fetch("http://localhost:3000/transactions")
+//             .then((response) => response.json())
+//             .then((data) => this.transactions = data)
+//     }
 }
 </script>
