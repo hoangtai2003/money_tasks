d0ed06ca14603e45cc7d689f660fa0f8305ee799
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2024-06-22 22:52:26 +0700

    Fetch one and loading

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1>Đây là trang chi tiết</h1>
-  <div class="item" v-for="transaction in transactions" :key="transaction.id">
-      <!-- <router-link :to="`transaction/${transaction.id}`">{{ transaction.name }}</router-link> -->
-       <router-link 
-            :to="{name: 'transactiondetail', 
-            params: {id: transaction.id}}"> {{ transaction.name }}
-       </router-link>
-      <div class="price">{{ transaction.price }}</div>
-  </div>
+    <div v-if="transactions.length">
+        <div class="item" v-for="transaction in transactions" :key="transaction.id">
+            <!-- <router-link :to="`transaction/${transaction.id}`">{{ transaction.name }}</router-link> -->
+            <router-link 
+                :to="{name: 'transactiondetail', 
+                params: {id: transaction.id}}"> {{ transaction.name }}
+            </router-link>
+            <div class="price">{{ transaction.price }}</div>
+        </div>
+    </div>
+  <div v-else>Loading transaction.....</div>
 </template>
 <script>
 export default {
@@ -18,17 +20,17 @@ export default {
     },
     // Cách 1: sử dụng await vì đây là dữ liệu bất đồng bộ
     // Sử dụng await trước một Promise nó sẽ tạm dừng thực thi của hàm cho đến khi Promise đó được giải quyết hoặc từ chối
-    async logTransactions() {
-        // Thực thi hàm sẽ tạm dừng ở đây cho đến khi yêu cầu fetch hoàn thành và Promise trả về bởi fetch được giải quyết với đối tượng phản hồi.
-        const response = await fetch("http://localhost:3000/transactions")
-        const data = await response.json()
-        this.transactions = data
-    }
+    // async created() {
+    //     // Thực thi hàm sẽ tạm dừng ở đây cho đến khi yêu cầu fetch hoàn thành và Promise trả về bởi fetch được giải quyết với đối tượng phản hồi.
+    //     const response = await fetch("http://localhost:3000/transactions")
+    //     const data = await response.json()
+    //     this.transactions = data
+    // }
     // Cách 2
-//     created() {
-//         fetch("http://localhost:3000/transactions")
-//             .then((response) => response.json())
-//             .then((data) => this.transactions = data)
-//     }
+    created() {
+        fetch("http://localhost:3000/transactions")
+            .then((response) => response.json())
+            .then((data) => this.transactions = data)
+    }
 }
 </script>
--- src/views/TransactionDetail.vue
+++ src/views/TransactionDetail.vue
@@ -1,5 +1,26 @@
 <template>
     <h1>Đây là trang chi tiết</h1>
     <!-- Lấy id -->
-    <span>{{ $route.params.id }}</span>
+     <div v-if="transaction">
+        <h1>ID: {{ $route.params.id }}</h1>
+        <h3>Name: {{ transaction.name }}</h3>
+        <p>Price: {{ transaction.price }}</p>
+     </div>
+     <div v-else>
+        <p>Loading transaction {{ $route.params.id  }} .....</p>
+     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                transaction: null
+            }
+        },
+        async created() {
+            const response = await fetch("http://localhost:3000/transactions/" + this.$route.params.id)
+            const data = await response.json()
+            this.transaction = data
+        }
+    }
+</script>
