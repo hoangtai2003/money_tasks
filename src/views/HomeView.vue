17ea0203b63c9e8c10854d45e6ad3f7b30c243e0
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2024-06-23 21:22:31 +0700

    ref and reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <input v-model="searchText">
+    </div>
+    <ul v-for="(customer, index) in customerFilter" :key="index">
+        <li>{{ customer }}</li>
+    </ul>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+import { ref, reactive, computed } from "vue";
+export default {  
+    setup() {
+        // Ref muốn truy cập thuộc tính cần phải truy cập thông qua value
+        const searchText = ref("")
+        // reactive thì truy cập trực tiếp vào đối tượng bên trong nó
+        // Reactive chỉ giúp thay đổi theo dõi bên trong object mà nó đã tạo
+        const customers = reactive([
+            "Hoàng Đức Tài",
+            "Sơn TÙng MTP",
+            "Đom Đóm"
+        ]);
+        const customerFilter = computed(() => {
+            customer.filter((customer) => customer.includes(searchText.value))
+        })
+        return {searchText, customerFilter}
+    }
 }
 </script>
